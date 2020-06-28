7eac6e4158e46518450dcacd2c00c6433445b3d8
Geoffrey Garrett <g.h.garrett13@gmail.com>
2020-06-28 22:09:11 +0200

    Alternative path concatenation for tudatpy issue#1
    - Set fixed sizes for each path.

--- include/tudat/resource/resource.h
+++ include/tudat/resource/resource.h
@@ -12,61 +12,65 @@
 #define SPACE_WEATHER "/space_weather"
 #define SPICE_KERNELS "/spice_kernels"
 
+#define MAX_PREFIX_LEN 255
+#define MAX_RESOURCE_LEN 10
+#define MAX_RELATIVE_LEN 20
+
 namespace fs = boost::filesystem;
 
 namespace tudat {
     namespace paths {
 
         static inline fs::path get_resources_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN];
             strcpy(path, Tudat_RESOURCE_DIR);
             return fs::path(path);
         }
 
         static inline fs::path get_atmosphere_tables_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, ATMOSPHERE_TABLES);
             return fs::path(path);
         }
 
         static inline fs::path get_earth_orientation_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, EARTH_ORIENTATION);
             return fs::path(path);
         }
 
         static inline fs::path get_ephemeris_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, EPHEMERIS);
             return fs::path(path);
         }
 
         static inline fs::path get_gravity_models_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, GRAVITY_MODELS);
             return fs::path(path);
         }
 
         static inline fs::path get_quadrature_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, QUADRATURE);
             return fs::path(path);
         }
 
         static inline fs::path get_space_weather_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, SPACE_WEATHER);
             return fs::path(path);
         }
 
         static inline fs::path get_spice_kernels_path() {
-            char path[255];
+            char path[MAX_PREFIX_LEN + MAX_RESOURCE_LEN + MAX_RELATIVE_LEN];
             strcpy(path, get_resources_path().c_str());
             strcat(path, SPICE_KERNELS);
             return fs::path(path);
