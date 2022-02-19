f883a03a6430418b53b61899a5c4a4904c610a58
Geoffrey <g.h.garrett13@gmail.com>
2022-02-19 15:58:08 +0200

    Added scrap file to trigger webservices

---
+++ test.py
@@ -0,0 +1,28 @@
+import subprocess
+
+regex = r"(?P<name>[\w,-]+)[\s]+(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?[\s]+(?P<build>[\w]+)[\s]+(?P<channel>[\w,-]+)"
+
+
+def search_conda_packages(channel, package):
+    cmd = ['conda', 'search']
+    if channel:
+        cmd.extend(['-c', channel])
+    cmd.append(package)
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    s = output.decode('ascii')
+    print(s)
+    res = []
+    # for s in s.split('\n'):
+    #
+    #     if s.startswith('conda-build'):
+    #         return s.split()[0]
+
+
+#     cmd1 = ['git', 'tag', '']
+# process = subprocess.Popen(, stdout=subprocess.PIPE)
+# output, error = process.communicate()
+
+
+if __name__ == "__main__":
+    search_conda_packages('tudat-team/dev', 'tudat-resources')
