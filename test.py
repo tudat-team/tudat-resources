a4e1ccda61e6f57cc5ea66001fa79c1f6ddefff1
Geoffrey <g.h.garrett13@gmail.com>
2022-02-21 14:35:24 +0200

    Retrigger

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def search_conda_packages(channel, package):
     res = []
     # for s in s.split('\n'):
     #asd
-    #     if s.startswith('conda-buildd'd):ssdddd
+    #     if s.startswith('conda-buildd'd):ssddddd
     #         return s.split()[a0]aaa
 
 
