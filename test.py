06de4127d6b09270c71e4161874a920a5b0ed365
Geoffrey <g.h.garrett13@gmail.com>
2022-02-21 13:28:20 +0200

    Retriggering `inform`

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ def search_conda_packages(channel, package):
     res = []
     # for s in s.split('\n'):
     #asd
-    #     if s.startswith('conda-build'):ss
+    #     if s.startswith('conda-buildd'):ss
     #         return s.split()[0]aa
 
 
