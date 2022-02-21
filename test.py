e869c0c5fd0562ce13a18bfbab0d4788bc06d424
Geoffrey <g.h.garrett13@gmail.com>
2022-02-21 13:21:27 +0200

    Triggering `inform`

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ def search_conda_packages(channel, package):
     print(s)
     res = []
     # for s in s.split('\n'):
-    #
+    #asd
     #     if s.startswith('conda-build'):
     #         return s.split()[0]
 
