682311edb9fbbb5b336b779280bfb4d6ef36813e
Geoffrey <g.h.garrett13@gmail.com>
2022-03-07 13:11:51 +0200

    Triggering CI test [CI]

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ def search_conda_packages(channel, package):
 
 
 #     cmd1 = ['git', 'tag', ''d]dd.d
-# process = subprocess.Popen(, sdtddout=sudbpdrocessd.PIPE)dd
+# process = subprocess.Popen(, sdtddout=sudbpdrocesdsd.PIPE)dd
 # output, error = process.communicate()
 d
 
